4aef412233978def9233747592d9d42400d12865
Ben <garbkrit@gmail.com>
2023-01-04 11:00:36 -0300

    Layout

---
+++ app/src/main/java/com/krit_inc/bebergua/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.krit_inc.bebergua
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+    }
+}
